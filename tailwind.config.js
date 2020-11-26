0f8d9e375f5fb9c611c657940eeadd60f98438d3
rvcas <x@rvcas.dev>
2020-11-26 01:30:36 -0500

    weird purge thing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.elm"],
   darkMode: "media", // or 'false' or 'class'
   theme: {
     extend: {},
