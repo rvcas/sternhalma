4afda26b52fa8348dfb70849584e509ecf7e594c
rvcas <x@rvcas.dev>
2020-11-25 22:08:40 -0500

    add some game logic

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
+import Array exposing (Array)
 import Browser exposing (Document)
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
@@ -23,8 +24,25 @@ main =
 -- Model
 
 
+type alias Player =
+    { id : Int
+    , score : Int
+    , color : String
+    }
+
+
+type alias Position =
+    { occupyingPlayer : Maybe Player
+    , scoringPlayer : Maybe Player
+    , index : Int
+    , row : Int
+    , col : Int
+    }
+
+
 type alias GameState =
     { totalPlayers : Int
+    , players : Array Player
     }
 
 
@@ -42,15 +60,37 @@ init =
 -- Update
 
 
+type alias PlayerId =
+    Int
+
+
 type Msg
     = SetPlayers Int
+    | SetScore PlayerId Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SetPlayers count ->
-            ( Playing { totalPlayers = count }, Cmd.none )
+            ( Playing { totalPlayers = count, players = Array.empty }, Cmd.none )
+
+        SetScore playerId score ->
+            case model of
+                Playing state ->
+                    let
+                        players =
+                            case Array.get playerId state.players of
+                                Just player ->
+                                    Array.set playerId { player | score = score } state.players
+
+                                Nothing ->
+                                    state.players
+                    in
+                    ( Playing { state | players = players }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
 
 
 
